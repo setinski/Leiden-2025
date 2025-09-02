e5463c1c89e1f992761e05afc7f2e05f542fb959
setinski <etinski.simona@gmail.com>
2025-09-02 10:41:15 +0200

    Undoing changes.

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 # --- Matrix / vector generators ---
 
-# Temporary changes
 def gen_checkerboard(n, k):
     """Return an n x k checkerboard of 0s and 1s."""
     pass
