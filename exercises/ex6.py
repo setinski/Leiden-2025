9650a25548075c2fa6fc7882d69d8f2260cccf67
Leo Ducas <ducas@cwi.nl>
2025-12-01 13:35:54 +0100

    almost full exercise 6

--- exercises/ex6.py
+++ exercises/ex6.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from math import sqrt, log, exp
+from math import sqrt, log, exp, ceil
 from sys import exit
 from sol5 import LLL
 
 # This function returns the basis of an SIS lattice
 def random_SIS_lattice(n, m, q):
-	B = np.block([[q * np.identity(m-n), np.zeros((m-n, n))],
-				  [np.random.randint(q, size=(n, m-n)), np.identity(n)]])
+	B = np.block([[q * np.identity(n), np.zeros((n, m-n))],
+				  [np.random.randint(q, size=(m-n, n)), np.identity(m-n)]])
 	return B
 
 
@@ -17,5 +17,22 @@ def random_SIS_lattice(n, m, q):
 def measure_rhf(n, m, q):
 	pass
 
-# Plot the root hermite factor of SIS(n, 2*n, 2**(3 + n/4))
-# for all n multiple of 5.
+# Plot the root hermite factor of SIS(n, 2*n, ceil(2**(3 + n/4)))
+# for all n from 10 to 50. (Also print them as they are computed
+# so that you can check intermediate results while the whole
+# plot is being computed which may take a few minutes)
+
+
+
+# Define C to be experimental limit of the root hermite factor
+# (The following should maatch your experiment roughly)
+
+C = 1.035 
+
+# Using C, n, m, q, predict the length of a solution to SIS using LLL:
+
+def predict_SIS_solution_length(n, m, q):
+
+
+
+	
