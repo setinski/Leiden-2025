2d82b57189662d92788bc0b6a796fcb8d4c3a683
Mirjam-N <m.noteborn@outlook.com>
2025-12-08 16:17:15 +0100

    ex7 done

--- exercises/ex7.py
+++ exercises/ex7.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt, log, exp, ceil
 from sys import exit
-from sol5 import LLL
-from sol6 import random_SIS_lattice
+from sol5 import *
+from sol6 import *
 
 
 # Measure the length of the first vector found by LLL on a 
@@ -18,31 +18,41 @@ def measure_SIS_solution_length(n, m, q):
 # prediction made in ex6 for n = 20, q=67 and m growing from 30 to 120.
 # using a logarithmic scale
 
-def plot_SIS():
-    result=[]
+def plot_SIS_and_profiles():
+    meas=[]
+    pred=[]
     rep=5
-    for i in range(30,100,5): #steps of 5
+    domain=range(30,110,5) #steps of 5
+    for i in domain: 
         x = 0
         for r in range(rep):
-            x += measure_SIS_solution_length(20, i, 67)
+            x += log(measure_SIS_solution_length(20, i, 67))
         print(i, x/rep)
-        result.append(x/rep)
-
-    plt.scatter(range(30,100,5), result)
+        meas.append(x/rep)
+        pred.append(log(predict_SIS_solution_length(20, i, 67)))
+    plt.plot(domain, meas)
+    plt.plot(domain, pred)
+    plt.scatter(domain, meas)
+    plt.scatter(domain, pred)
     plt.xlim(30, 100)
     plt.xlabel("m")
-    plt.ylabel("prediction")
-    plt.title("SIS")
 
+    plt.show()
+
+    for i in [30, 60, 100]:
+        B = random_SIS_lattice(20, i, 67)
+        list(LLL(B))
+        _, D, _ = gram_schmidt_decomposition(B)
+        plt.plot(range(i), np.log(np.abs(D)))
     plt.show()
     return
 
 #Try to understand what is going on by looking at the profile
-# of a basis for relevant values of m.
+# of a basis for relevant values of m=30,60,100.
 
 
 ############
 # Main runner
 ############ 
 if __name__ == "__main__":
-    plot_SIS()
+    plot_SIS_and_profiles()
