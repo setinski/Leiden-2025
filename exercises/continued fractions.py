de9b4043b2d8adf7f9257ed0d33c61bc9d6357eb
wall-e-droid <mario.mascolo00@outlook.it>
2026-01-15 22:48:35 +0100

    added new tiling algorithm

--- exercises/continued fractions.py
+++ exercises/continued fractions.py
@@ -1,15 +1,16 @@
+import math
 # Lagrange Reduction coincides with euclid's gcd algorithm if the input given is two parallel vectors.
 # In particular, it can be used to compute the continued fraction expansion of a given number.
 
-a = 12
-b = 25
+a = 23
+b = 2026
 
 def LagrangeRed(a, b):
     condition = True
     lista = []
     while condition:
         a,b = b,a
-        k = int(round(b/a))
+        k = int(math.floor(b/a)) # round does not quite work
         lista.append(k)
         b -= k*a
         condition = abs(a) > abs(b) and b!= 0
